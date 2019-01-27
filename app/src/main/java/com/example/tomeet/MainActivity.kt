138c786f5f93f196dfb7d5698faa3abfa6058844
정빈 <qlsl3574@naver.com>
2019-01-27 23:07:02 +0900

    bottomAppbar-test1

--- app/src/main/java/com/example/tomeet/MainActivity.kt
+++ app/src/main/java/com/example/tomeet/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.example.tomeet
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item!!.itemId) {
+            R.id.app_bar_fav -> Toast.makeText(this,"프로필을 눌르셨습니다 !",Toast.LENGTH_LONG).show()
+            R.id.app_bar_search -> Toast.makeText(this,"검색버튼 을 눌르셨습니다 !",Toast.LENGTH_LONG).show()
+            R.id.app_bar_settings -> Toast.makeText(this,"셋팅 을 눌르셨습니다 !",Toast.LENGTH_LONG).show()
+        }
+        return true
     }
 }

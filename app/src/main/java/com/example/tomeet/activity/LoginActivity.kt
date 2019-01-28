04b17e661f8ea1cdacba28535d38f0206e6ba743
정빈 <qlsl3574@naver.com>
2019-01-29 07:57:48 +0900

    뒤로가기 2번 누를시 종료 & registerButton

--- app/src/main/java/com/example/tomeet/activity/LoginActivity.kt
+++ app/src/main/java/com/example/tomeet/activity/LoginActivity.kt
@@ -1,16 +1,42 @@
 package com.example.tomeet.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import com.example.tomeet.R
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    private val FINSH_INTERVAL_TIME = 2000
+    private var backPressedTime:Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         lottie_login.playAnimation()
+
+        register()
+
+    }
+
+    private fun register(){
+        button_register.setOnClickListener {
+            startActivity(Intent(this@LoginActivity, RegisterActivity::class.java))
+        }
+    }
+
+    override fun onBackPressed() {
+        var tempTime = System.currentTimeMillis()
+        var intervalTime = tempTime - backPressedTime
+        if (intervalTime in 0..FINSH_INTERVAL_TIME) {
+            ActivityCompat.finishAffinity(this)
+        } else {
+            backPressedTime = tempTime
+            Toast.makeText(applicationContext, "한번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT).show()
+        }
     }
 }
